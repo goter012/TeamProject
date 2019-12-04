58dc424a35a81f1b5647384fed07f793b98dda50
Gemma Velasco <gemmavelasco@gemmas-macbook-air.local>
2019-12-03 19:08:33 -0500

    Still need to finish camera

---
+++ TeamProject/ImageStore.swift
@@ -0,0 +1,28 @@
+//
+//  ImageStore.swift
+//  TeamProject
+//
+//  Created by Gemma Velasco on 12/3/19.
+//  Copyright © 2019 guillermo j otero jr. All rights reserved.
+//
+
+import UIKit
+
+class ImageStore {
+    
+    let cache = NSCache<NSString, UIImage>()
+    
+    func setImage(_ image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key as NSString)
+    }
+    
+    func image(forKey key: String) -> UIImage? {
+        return cache.object(forKey: key as NSString)
+    }
+    
+    func deleteImage(forKey key: String) {
+        cache.removeObject(forKey: key as NSString)
+    }
+    
+}
+
--- TeamProject/profileViewController.swift
+++ TeamProject/profileViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 import CoreData
 
-class profileViewController: UIViewController{
+class profileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     @IBOutlet var userPicture: UIImageView!
     @IBOutlet var nameField: UITextField!
@@ -20,7 +20,7 @@ class profileViewController: UIViewController{
    
     var email: String!
     var password: String!
-    var imageStore: UIImage!
+    var imageStore: ImageStore!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -28,5 +28,42 @@ class profileViewController: UIViewController{
         self.navigationController?.setNavigationBarHidden(false, animated: true)
         emailField.text = email
         
+    }
+    
+    @IBAction func cameraPressed(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        
+        // If the device has a camera, take a picture, otherwise,
+        // just pick from photo library
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        // Place image picker on the screen
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String: Any]) {
+        
+        // Get picked image from info dictionary
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Store the image in the ImageStore for the item's key
+        imageStore.setImage(image, forKey: "User" )
+        
+        // Put that image onto the screen in our image view
+        userPicture.image = image
+        
+        // Take image picker off the screen -
+        // you must call this dismiss method
+        dismiss(animated: true, completion: nil)
     }
 }
